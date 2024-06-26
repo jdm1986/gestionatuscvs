189e9263a0232cf2c4c0ab71c9075828bdb0d0e7
sportyisus <diazmartinmail@gmail.com>
2024-06-26 18:25:47 +0200

    Modificaciones para que arranque la aplicación correctamente

--- src/main/java/com/example/gestion_curriculums0/GestionCurriculums0Application.java
+++ src/main/java/com/example/gestion_curriculums0/GestionCurriculums0Application.java
@@ -2,12 +2,13 @@ package com.example.gestion_curriculums0;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.gestion_curriculums0")
 public class GestionCurriculums0Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionCurriculums0Application.class, args);
 	}
 }
-
--- src/main/java/com/example/gestion_curriculums0/Usuario.java
+++ src/main/java/com/example/gestion_curriculums0/Usuario.java
@@ -1,15 +1,11 @@
 package com.example.gestion_curriculums0;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Column;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Usuarios")
 public class Usuario {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

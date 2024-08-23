91b68428c323b9b5560b11dabffc979c7a9f6750
sportyisus <diazmartinmail@gmail.com>
2024-08-23 19:58:37 +0200

    ajuste de textos email inclusivos y reajuste del check box para no desplazar texto en dispositivos móviles.

--- src/main/java/com/example/gestion_curriculums0/service/EmailService.java
+++ src/main/java/com/example/gestion_curriculums0/service/EmailService.java
@@ -17,8 +17,8 @@ public class EmailService {
     public void sendWelcomeEmail(String to, String username) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
-        message.setSubject("Bienvenido a nuestra plataforma");
-        message.setText("Hola " + username + ",\n\n¡Bienvenido a nuestra plataforma! Estamos encantados de tenerte con nosotros.\n\nSaludos,\nEl equipo");
+        message.setSubject("Bienvenid@ a nuestra plataforma");
+        message.setText("Hola " + username + ",\n\n¡Bienvenid@ a nuestra plataforma! Estamos encantados de tenerte con nosotros.\n\nSaludos,\nEl equipo");
 
         try {
             mailSender.send(message);
